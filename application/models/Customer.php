350550a1d903679124ff693432f779d588f61778
jger-j <mendejaj@gmail.com>
2018-08-09 01:19:47 -0700

    Initialized files

---
+++ application/models/Customer.php
@@ -0,0 +1,16 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+
+/**
+ * Description of Customer
+ *
+ * @author End 
+ */
+class Customer {
+    //put your code here
+}
---
+++ application/models/Payment.php
@@ -0,0 +1,16 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+
+/**
+ * Description of Payment
+ *
+ * @author End 
+ */
+class Payment {
+    //put your code here
+}
